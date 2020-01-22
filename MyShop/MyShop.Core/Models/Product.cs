d81ed04bfd79cfdad168b445bf9c55a54e93f715
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2020-01-22 15:47:49 -0300

    Implemented generic Repository

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -3,19 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyShop.Core.Models
 {
-    public class Product
-    {
-        public string Id { get; set; }
+    public class Product:BaseEntity
+    {   
         [StringLength(20)]
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
         public string Image { get; set; }
-
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
+        
     }
 }
